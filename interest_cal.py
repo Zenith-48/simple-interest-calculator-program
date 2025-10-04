41fff9816399e2ff5dafbe433b2b025102dfa2ea
Zenith-48 <shalinamaniyar@gmail.com>
2025-10-04 09:33:02 +0530

    Update interest_cal.py

--- interest_cal.py
+++ interest_cal.py
@@ -4,3 +4,6 @@ time = float(input("Enter the time (in years): "))
 
 simple_interest = (principal * rate * time) / 100
 print("\nSimple Interest =", simple_interest)
+
+compound_interest = principal * (1 + rate / 100) ** time - principal)
+print("\nCompound Interest =", compound_interest)
